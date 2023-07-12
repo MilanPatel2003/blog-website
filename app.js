4d77984c8684c70ccb4533598f67e7a23b9b6a24
MilanPatel2003 <milanpatel6454@gmail.com>
2023-07-12 20:13:55 +0530

    database added

--- app.js
+++ app.js
@@ -2,18 +2,45 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
-
+const { default: mongoose } = require("mongoose");
 const app = express();
 
-app.set("view engine", "ejs");
+async function main(){
+    try{
+        db = await mongoose.connect("mongodb+srv://milanpatel6454:Greedygamer007@allmightymongo.cbeekum.mongodb.net/blogDB?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("connected to database server...");
+} catch(err){
+    console.log("failed to connect to the database");
+}
+}
+main();
 
+
+
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const allBlogs = [];
+
+
+const blogSchema = new mongoose.Schema({
+  title:String,
+  body:String
+});
+
+const blog = mongoose.model("blog",blogSchema);
+
 
 app.get("/", (req, res) => {
-  res.render("home", { allBlogs: allBlogs });
+
+  blog.find({}).then(function (allBlogs) {
+    res.render("home", { blogs: allBlogs});
+    })
+
+
+  // res.render("home", { allBlogs: allBlogs });
+
+
 });
 app.get("/about", (req, res) => {
   res.render("about");
@@ -24,25 +51,34 @@ app.get("/contact", (req, res) => {
 app.get("/compose", (req, res) => {
   res.render("compose");
 });
-app.get("/posts/:title", (req, res) => {
-  const title = req.params.title;
+app.get("/posts/:blogid", (req, res) => {
+  // const title = req.params.title;
+  // allBlogs.forEach((blogs) => {
+  //   if (lodash.lowerCase(blogs.title) === lodash.lowerCase(title)) {
+  //     res.render("post", { title: blogs.title, body: blogs.body });
+  //   }
+  // });
+  const currentBlogId = lodash.slice(req.params.blogid,1).join('');
+  console.log(currentBlogId);
 
-  allBlogs.forEach((blogs) => {
-    if (lodash.lowerCase(blogs.title) === lodash.lowerCase(title)) {
-      res.render("post", { title: blogs.title, body: blogs.body });
-    }
-  });
+  blog.find({_id:currentBlogId}).then(function (foundBlog) {
+    res.render("post",{currentBlog:foundBlog})
+    })
+  
 });
 
 app.post("/compose", (req, res) => {
-  const post = {
+  const post =new blog( {
     title: req.body.postTitle,
     body: req.body.postBody,
-  };
+  });
 
-  allBlogs.push(post);
-
-  res.redirect("/");
+  post.save().then(function () { 
+    res.redirect("/");
+    console.log("blog inserted in db");
+   })
+  
+  
 });
 
 app.listen(3000, function () {
